952aba3564f46121615a84a4893b3035ee2a4c8c
Justin <jusitnlightsaber@gmail.com>
2017-08-25 16:48:24 -0700

    First commit. Currently a basic dice roller and a few buttons.

---
+++ js/dice.js
@@ -0,0 +1,26 @@
+
+var selectedSides = $(this)
+
+var dice = {
+	sides: selectedSides,
+	roll: function () {
+		var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+		return randomNumber;
+	}
+
+}
+
+
+function printNumber(number) {
+  var placeholder = document.getElementById("placeholder");
+  placeholder.innerHTML = number;
+}
+
+var button = document.getElementById("button");
+
+button.onclick = function() {
+  var result = dice.roll();
+  printNumber(result);
+};
+
+
