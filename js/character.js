12c58c11ebe8729766de11e656ee4fbc9e7ce056
Justin <jusitnlightsaber@gmail.com>
2017-09-27 16:29:33 -0700

    Added random skills

--- js/character.js
+++ js/character.js
@@ -1,5 +1,12 @@
 var randomCharacter = [];
-var classList = ["Fighting Man", "Magic User", "Cleric", "Elf", "Dwarf", "Hobbit"]
+var classList = ["Fighter", "Magic User", "Cleric", "Elf", "Dwarf", "Hobbit", "Mystic", "Thief"];
+var abilityBonus = [0, -4, -3, -3, -2, -2, -1, -1, -1, 0, 0, 0, 0, 1, 1, 1, 2, 2, 3];
+var skills = ["Arcane Lore", "Balance", "Bluff", "Cooking", "craft", "Diplomacy", "Disguise", "Engineering", "Escape Artist", "Etiquette (pick a culture)", "First Aid", "Gambling", "Geography", "History", "Intimidation", "Jumping", "Language (pick a language)", "Laws (pick a culture)", "Lip Reading", "Magical Engineering", "Nature Lore", "Navigating", "performance", "Religious Lore", "riding", "Sense Motive", "Swimming", "Tracking"];
+var craft = ["Carpentry", "Smithing", "Fletching", "Skinning", "Leatherworking", "Rope Binding", "Tailoring", "Gem Cutting", "Forgery", "Masonry", "Thatching", "Drawing/Painting", "Sculpture"];
+var performance = ["Singing", "Playing an instrument", "Dancing", "Acting", "Juggling", "Jesting", "Storytelling", "Poetry"];
+var riding = ["Riding - Horse", "Riding - Elephant", "Riding - Dire Wolf", "Riding - Griffon", "Riding - Pegasus", "Riding - Hippogriff", "Riding - Giant Eagle"];
+var skillPicked;
+var skillHold = [];
 
 
 
@@ -14,10 +21,14 @@ function Dice(sides, n) {
   
 
 var random6 = new Dice(6, 1);
+var random8 = new Dice(8, 1);
+var randomCraft = new Dice(13, 1);
+var randomPerformance = new Dice(8, 1);
+var randomRiding = new Dice(7, 1);
 
 
 function printCharacter(character) {
-  var setCharacter = ["class", "str", "int", "wis", "con", "dex", "cha"]
+  var setCharacter = ["class", "str", "strBonus", "int", "intBonus", "wis", "wisBonus", "con", "conBonus", "dex", "dexBonus", "cha", "chaBonus", "attackBonus", "skill1", "skill2", "skill3", "skill4"]
   for (var i = 0; i < setCharacter.length; i++) {
   	var setClass = document.getElementById(setCharacter[i]);
   	setClass.innerHTML = character[i];
@@ -31,7 +42,7 @@ button.onclick = function() {
 	
 	//find class
 	randomCharacter = [];
-  	var result = random6.roll();
+  	var result = random8.roll();
   	randomCharacter.push(classList[result - 1])
   	//find stats
   	for (i=0; i < 6; i++) {
@@ -43,9 +54,35 @@ button.onclick = function() {
   			( acc, cur ) => acc + cur,
   			0
 		);
+		var statBonus = abilityBonus[stat];
   		randomCharacter.push(stat);
+  		randomCharacter.push(statBonus);
 
   	}
+  	//attack bonus is always 1
+  	randomCharacter.push(1);
 
+  	//random skills
+  	for (var i = 0; i < 4; i++) {
+  		skillPicked = skills[i];
+  		skillHold.push(skillPicked);
+  		console.log(skillPicked);
+  		skills.splice(i, 1);
+  		if (skillPicked === "craft") {
+  			skillPicked = craft[randomCraft.roll()];
+  		}
+  		if (skillPicked === "performance") {
+  			skillPicked = performance[randomPerformance.roll()];
+  		}
+  		if (skillPicked === "riding" ) {
+  			skillPicked = riding[randomRiding.roll()];
+  		}
+  		
+  		randomCharacter.push(skillPicked);
+  	}
+  	Array.prototype.push.apply(skills, skillHold);
+  	skillHold.splice(0)
+  	console.log(skills);
+  	console.log(randomCharacter);
   	printCharacter(randomCharacter);
 };
