040dcce93210eebdc0ec29e191fd7edf5b6b4c71
Justin <jusitnlightsaber@gmail.com>
2017-09-29 15:22:43 -0700

    Adding random roll for weapon feats and weapons, not currently working

--- js/character.js
+++ js/character.js
@@ -1,14 +1,22 @@
 var randomCharacter = [];
-var classList = ["Fighter", "Magic User", "Cleric", "Elf", "Dwarf", "Hobbit", "Mystic", "Thief"];
+var classList = ["Fighter", "Magic User", "Cleric", "Elf", "Dwarf", "Halfling", "Mystic", "Thief"];
+var classIndex;
 var abilityBonus = [0, -4, -3, -3, -2, -2, -1, -1, -1, 0, 0, 0, 0, 1, 1, 1, 2, 2, 3];
 var skills = ["Arcane Lore", "Balance", "Bluff", "Cooking", "craft", "Diplomacy", "Disguise", "Engineering", "Escape Artist", "Etiquette (pick a culture)", "First Aid", "Gambling", "Geography", "History", "Intimidation", "Jumping", "Language (pick a language)", "Laws (pick a culture)", "Lip Reading", "Magical Engineering", "Nature Lore", "Navigating", "performance", "Religious Lore", "riding", "Sense Motive", "Swimming", "Tracking"];
 var craft = ["Carpentry", "Smithing", "Fletching", "Skinning", "Leatherworking", "Rope Binding", "Tailoring", "Gem Cutting", "Forgery", "Masonry", "Thatching", "Drawing/Painting", "Sculpture"];
 var performance = ["Singing", "Playing an instrument", "Dancing", "Acting", "Juggling", "Jesting", "Storytelling", "Poetry"];
 var riding = ["Riding - Horse", "Riding - Elephant", "Riding - Dire Wolf", "Riding - Griffon", "Riding - Pegasus", "Riding - Hippogriff", "Riding - Giant Eagle"];
-var skillPicked;
-var skillHold = [];
-
-
+var picked;
+var hold = [];
+var weapons = [["Hand Axe", "Blackjack", "Club", "Cestus", "Dagger", "Throwing Hammer", "War Hammer", "Javelin", "Lance", "Mace", "Net", "Spear", "Bastard Sword", "Normal Sword", "Short Sword", "Trident", "Whip", "Battle Axe", "Halberd", "Pike", "Poleaxe", "Staff", "Two-Handed Sword", "Horned Shield", "Knife Shield", "Sword Shield", "Tusked Shield", "Large Blowgun", "Small Blowgun", "Bolas", "Long Bow", "Short Bow", "Heavy Crossbow", "Light Crossbow", "Pistol", "Sling", "Smoothbore"],
+				["Dagger", "Net", "Whip", "Staff", "Large Blowgun", "Small Blowgun", "Pistol", "Sling"],
+				["Blackjack", "Club", "Throwing Hammer", "War Hammer", "Mace", "Staff", "Sling"],
+				["Hand Axe", "Blackjack", "Club", "Cestus", "Dagger", "Throwing Hammer", "War Hammer", "Javelin", "Lance", "Mace", "Net", "Spear", "Bastard Sword", "Normal Sword", "Short Sword", "Trident", "Whip", "Battle Axe", "Halberd", "Pike", "Poleaxe", "Staff", "Two-Handed Sword", "Horned Shield", "Knife Shield", "Sword Shield", "Tusked Shield", "Large Blowgun", "Small Blowgun", "Bolas", "Long Bow", "Short Bow", "Heavy Crossbow", "Light Crossbow", "Pistol", "Sling", "Smoothbore"],
+				["Hand Axe", "Blackjack", "Cestus", "Dagger", "Throwing Hammer", "War Hammer", "Javelin", "Mace", "Net", "Normal Sword", "Short Sword", "Trident", "Whip", "Battle Axe", "Staff", "Horned Shield", "Knife Shield", "Sword Shield", "Large Blowgun", "Small Blowgun", "Bolas", "Short Bow", "Light Crossbow", "Pistol", "Sling"],
+				["Hand Axe", "Blackjack", "Cestus", "Dagger", "Short Sword", "Horned Shield", "Knife Shield", "Sword Bow", "Light Crossbow", "Pistol"],
+				["Hand Axe", "Blackjack", "Club", "Cestus", "Dagger", "Throwing Hammer", "War Hammer", "Javelin", "Lance", "Mace", "Net", "Spear", "Bastard Sword", "Normal Sword", "Short Sword", "Trident", "Whip", "Battle Axe", "Halberd", "Pike", "Poleaxe", "Staff", "Two-Handed Sword", "Large Blowgun", "Small Blowgun", "Bolas", "Long Bow", "Short Bow", "Heavy Crossbow", "Light Crossbow", "Pistol", "Sling", "Smoothbore"],
+				["Hand Axe", "Blackjack", "Club", "Cestus", "Dagger", "Throwing Hammer", "War Hammer", "Javelin", "Lance", "Mace", "Net", "Spear", "Bastard Sword", "Normal Sword", "Short Sword", "Trident", "Whip", "Large Blowgun", "Small Blowgun", "Bolas", "Long Bow", "Short Bow", "Heavy Crossbow", "Light Crossbow", "Pistol", "Sling"]];
+var arrayLength;
 
 function Dice(sides, n) {
   this.sides = sides +1;
@@ -25,6 +33,7 @@ var random8 = new Dice(8, 1);
 var randomCraft = new Dice(13, 1);
 var randomPerformance = new Dice(8, 1);
 var randomRiding = new Dice(7, 1);
+var randomArray = new Dice(arrayLength, 1);
 
 
 function printCharacter(character) {
@@ -43,6 +52,7 @@ button.onclick = function() {
 	//find class
 	randomCharacter = [];
   	var result = random8.roll();
+  	classIndex = result-1;
   	randomCharacter.push(classList[result - 1])
   	//find stats
   	for (i=0; i < 6; i++) {
@@ -64,25 +74,53 @@ button.onclick = function() {
 
   	//random skills
   	for (var i = 0; i < 4; i++) {
-  		skillPicked = skills[i];
-  		skillHold.push(skillPicked);
-  		console.log(skillPicked);
+  		
+  		arrayLength = Number(skills.length);
+		var rollResult = randomArray.roll()
+  		picked = skills[rollResult];
+  		hold.push(picked);
   		skills.splice(i, 1);
-  		if (skillPicked === "craft") {
-  			skillPicked = craft[randomCraft.roll()];
+  		if (picked === "craft") {
+  			picked = craft[randomCraft.roll()];
   		}
-  		if (skillPicked === "performance") {
-  			skillPicked = performance[randomPerformance.roll()];
+  		if (picked === "performance") {
+  			picked = performance[randomPerformance.roll()];
   		}
-  		if (skillPicked === "riding" ) {
-  			skillPicked = riding[randomRiding.roll()];
+  		if (picked === "riding" ) {
+  			picked = riding[randomRiding.roll()];
   		}
   		
-  		randomCharacter.push(skillPicked);
+  		randomCharacter.push(picked);
   	}
-  	Array.prototype.push.apply(skills, skillHold);
-  	skillHold.splice(0)
-  	console.log(skills);
+
+  	
+  	Array.prototype.push.apply(skills, hold);
+	hold.splice(0);
+  	
+
+  	//random weapon and weapon feats
+  	/*var feats;
+  	var classWeapon = classIndex;
+  	arrayLength = weapons[classWeapon].length;
+  	if (classWeapon === 0) {
+  		feats = 4;
+  	}
+  	else { 
+  		feats = 2;
+  	}
+
+  	for (var i = 0; i < feats; i++) {
+  		picked = skills[i];
+  		hold.push(picked);
+  		skills.splice(i, 1);
+  	}*/
+
+
+
+
+
+
+  	
   	console.log(randomCharacter);
   	printCharacter(randomCharacter);
 };
